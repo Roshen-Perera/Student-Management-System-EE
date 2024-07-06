bbe527819c762b931fb21985da17e38565b4a08a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2024-07-06 10:13:30 +0530

    added comments

--- src/main/java/lk/ijse/studentmanagementsystemee/StudentController.java
+++ src/main/java/lk/ijse/studentmanagementsystemee/StudentController.java
@@ -19,7 +19,7 @@ public class StudentController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        /*Todo:*/
+        /*Todo*/
         if (!req.getContentType().toLowerCase().startsWith("application/json")) {
             /*Send Error*/
             resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
@@ -34,14 +34,21 @@ public class StudentController extends HttpServlet {
 //        writer.write(sb.toString());
 //        writer.close();
 
-//      JSON manipulate with Parson
+
         PrintWriter writer = resp.getWriter();
-        JsonReader reader = Json.createReader(req.getReader());
-        JsonObject jsonObject = reader.readObject();
-        System.out.println(jsonObject.toString());
-        System.out.println("Name: "+jsonObject.getString("name"));
-        System.out.println("Address: "+jsonObject.getString("address"));
-        System.out.println("Institute: "+jsonObject.getString("institute"));
+
+//      JSON manipulate with Parson
+        JsonReader reader = Json.createReader(req.getReader()); //Reads the request and passes to a special type reader called Json reader.
+        JsonObject jsonObject = reader.readObject(); // Reads the object and assigns it to a JsonObject type variable.
+        System.out.println(jsonObject.toString()); //Prints the JsonObject variable to the console
+
+        System.out.println("Name: "+jsonObject.getString("name")); //Prints a specific data from a JsonObject variable to the console
+        System.out.println("Address: "+jsonObject.getString("address")); //Prints a specific data from a JsonObject variable to the console
+        System.out.println("Institute: "+jsonObject.getString("institute")); //Prints a specific data from a JsonObject variable to the console
+
         writer.write(jsonObject.toString());
+        writer.write("\n");
+        writer.write(jsonObject.getString("name"));
+        System.out.println(jsonObject.getString("name"));
     }
 }
